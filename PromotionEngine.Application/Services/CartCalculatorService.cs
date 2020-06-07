575c0eadfb2551b38fe4bc59698cedb5cd839b93
gowtham Kumar kolagani <gowtham.developer@outlook.com>
2020-06-07 16:51:42 +0530

    added Cart Calculator Definations for business logic

---
+++ PromotionEngine.Application/Services/CartCalculatorService.cs
@@ -0,0 +1,21 @@
+﻿using PromotionEngine.Application.Contracts;
+using PromotionEngine.Domain.Cart;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PromotionEngine.Application.Services
+{
+    class CartCalculatorService : ICartCaluclatorService
+    {
+        public double GetProductSetBasedPromotionValue(CartItem cartItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double GetProductvalueIncart(CartItem cartItem)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
