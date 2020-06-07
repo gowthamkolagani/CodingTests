b3625b4f3a0575ef2891067a55a8e31833aaaba8
gowtham Kumar kolagani <gowtham.developer@outlook.com>
2020-06-07 16:42:57 +0530

    added domain layer and domains

--- PromotionEngine.Domain/Cart/Promotion/IPromotionHelper.cs
+++ PromotionEngine.Domain/Cart/Promotion/IPromotionHelper.cs
@@ -2,12 +2,11 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace PromotionEngine.Domain.CheckOut.Promotion
+namespace PromotionEngine.Domain.Cart.Promotion
 {
    public interface IPromotionHelper
     {
         Tuple<int, int> GetPromotionItemSets(int nuberofProducts, int offerSetValue);
         
-
     }
 }
